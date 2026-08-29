00a573e8b8cdf1663341023f9c6dd46de493e066
matheus <matheusotto1906@gmail.com>
2026-08-29 20:08:04 -0300

    feat: Implementado parcialmente app.py, posteriormente colocar as rotas

---
+++ src/app.py
@@ -0,0 +1,35 @@
+
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+from fastapi import FastAPI
+import sys
+
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+
+
+app = FastAPI(
+    title="Mallverse API",
+    description="Projeto par gerenciamento de shoppings",
+    version="0.1.0",
+)
+
+origins = [
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("src.app:app", host="127.0.0.1", port=8000, reload=True)
+
