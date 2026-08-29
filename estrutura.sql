3a96ceb688331ae522a25f94fda63dd78779783e
matheus <matheusotto1906@gmail.com>
2026-08-29 14:09:56 -0300

    fix: arrumado a estrutura do banco de dados

--- estrutura.sql
+++ estrutura.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS nexus_shoppings;
+DROP DATABASE IF EXISTS mallverse_db;
 
-CREATE DATABASE nexus_shoppings;
+CREATE DATABASE mallverse_db;
 
-USE nexus_shoppings;
+USE mallverse_db;
 
 CREATE TABLE shoppings (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -12,8 +12,19 @@ CREATE TABLE shoppings (
     registro_ativo BIT NOT NULL DEFAULT(1)
 );
 
+CREATE TABLE lojas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_fantasia VARCHAR(100) NOT NULL,
+    numero_modulo VARCHAR(20) NOT NULL,
+    id_shopping INT NOT NULL,
+    FOREIGN KEY (id_shopping) REFERENCES shoppings(id),
+    registro_ativo BIT NOT NULL DEFAULT(1)
+);
+
 CREATE TABLE funcionarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
+    id_loja INT NOT NULL,
+    FOREIGN KEY (id_loja) REFERENCES lojas(id),
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
     cargo VARCHAR(50) NOT NULL,
@@ -29,14 +40,6 @@ CREATE TABLE clientes (
 );
 
 
-CREATE TABLE lojas (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_fantasia VARCHAR(100) NOT NULL,
-    numero_modulo VARCHAR(20) NOT NULL,
-    id_shopping INT,
-    FOREIGN KEY (id_shopping) REFERENCES shoppings(id),
-    registro_ativo BIT NOT NULL DEFAULT(1)
-);
 
 CREATE TABLE estacionamento (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -51,7 +54,7 @@ CREATE TABLE avaliacoes (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nota INT NOT NULL,
     comentario TEXT,
-    id_cliente INT,
+    id_cliente INT NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES clientes(id),
     registro_ativo BIT NOT NULL DEFAULT(1)
 );
@@ -61,7 +64,9 @@ CREATE TABLE contratos (
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
     valor_aluguel FLOAT NOT NULL,
-    id_loja INT,
+    id_loja INT NOT NULL,
     FOREIGN KEY (id_loja) REFERENCES lojas(id),
+    id_shopping INT NOT NULL,
+    FOREIGN KEY (id_shopping) REFERENCES shoppings(id),
     registro_ativo BIT NOT NULL DEFAULT(1)
 );
