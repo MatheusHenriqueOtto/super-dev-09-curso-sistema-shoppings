1b4c50785b082cc2a55f368845efd9caad9d4497
matheus <matheusotto1906@gmail.com>
2026-08-30 08:41:51 -0300

    fix: Correção na funcão de consultar todos, onde mostrava os registros inativados, agora mostra somente os registros ativos

--- src/repositories/shoppings_repository.py
+++ src/repositories/shoppings_repository.py
@@ -4,13 +4,26 @@ from src.schemas.shoppings import Shopping
 def consultar_todos() -> list[Shopping] | None:
     with conectar() as conexao:
         with conexao.cursor() as cursor:
-            cursor.execute("SELECT id, nome, cnpj, cidade, registro_ativo FROM shoppings")
+            sql = """SELECT 
+                id, 
+                nome, 
+                cnpj, 
+                cidade, 
+                CAST(registro_ativo AS UNSIGNED) AS registro_ativo
+            FROM shoppings
+            WHERE registro_ativo = b'1';"""
+            cursor.execute(sql)
             registros: list = cursor.fetchall()
 
 
     shoppings: list[Shopping] = []
     for registro in registros:
-        shopping = Shopping(id=registro[0], nome=registro[1], cnpj=registro[2], cidade=registro[3], registro_ativo=registro[4])
+        shopping = Shopping(
+            id=registro[0], 
+            nome=registro[1], 
+            cnpj=registro[2], 
+            cidade=registro[3], 
+            registro_ativo=registro[4])
         shoppings.append(shopping)
 
     return shoppings
